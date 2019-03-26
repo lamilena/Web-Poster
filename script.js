ae7e69b9213ea2c98521667d9b16c9dc6f9cb443
lamilena <laminahelena@gmail.com>
2019-03-26 14:35:07 +0300

    new
    new

--- script.js
+++ script.js
@@ -64,32 +64,3 @@ function createStars() {
 $(function(){
   createStars()
 });
-
-let xs = []
-for (var i = 0; i <= 500; i++) {
-   xs.push(i)
-}
-
-let t = 0
-
-function animate() {
-
-   let points = xs.map(x => {
-
-     let y = 200 + 20 * Math.sin((x + t)/10)
-
-     return [x, y]
-   })
-
-   let path = "M" + points.map(p => {
-     return p[o] + "," + p[1]
-   }).join("L")
-
-   document.querySelector("path").setAttribute("d",path)
-
-   t += 0.5
-
-   requestAnimationFrame(animate)
-}
-
-animate()
